6787ceba889b1b204df71b190379f244c6683eb1
Q Trang <devqapps@gmail.com>
2020-08-04 00:34:54 -0700

    Changed indigo colors to static

--- Sources/SwiftUIStyleKit/Colors/Color/Color+Indigo.swift
+++ Sources/SwiftUIStyleKit/Colors/Color/Color+Indigo.swift
@@ -8,39 +8,39 @@
 import SwiftUI
 
 extension Color {
-    public var indigo100: Color {
+    public static var indigo100: Color {
         Color(UIColor.indigo100)
     }
     
-    public var indigo200: Color {
+    public static var indigo200: Color {
         Color(UIColor.indigo200)
     }
     
-    public var indigo300: Color {
+    public static var indigo300: Color {
         Color(UIColor.indigo300)
     }
     
-    public var indigo400: Color {
+    public static var indigo400: Color {
         Color(UIColor.indigo400)
     }
     
-    public var indigo500: Color {
+    public static var indigo500: Color {
         Color(UIColor.indigo500)
     }
     
-    public var indigo600: Color {
+    public static var indigo600: Color {
         Color(UIColor.indigo600)
     }
     
-    public var indigo700: Color {
+    public static var indigo700: Color {
         Color(UIColor.indigo700)
     }
     
-    public var indigo800: Color {
+    public static var indigo800: Color {
         Color(UIColor.indigo800)
     }
     
-    public var indigo900: Color {
+    public static var indigo900: Color {
         Color(UIColor.indigo900)
     }
 }
