1d7620b109a4c60d80f6b3417c2b05695fe1cbef
Q Trang <devqapps@gmail.com>
2020-09-10 14:23:18 -0700

    Added indicator color as argument for LoadingView

--- Sources/SwiftUIStyleKit/Modifiers/ActivityIndicator/LoadingView.swift
+++ Sources/SwiftUIStyleKit/Modifiers/ActivityIndicator/LoadingView.swift
@@ -11,12 +11,18 @@ public struct LoadingView: View {
     @Binding public var isAnimating: Bool
     public let text: String
     public let textColor: Color
+    public let indicatorColor: UIColor
     public var size: CGSize
     
-    public init(isAnimating: Binding<Bool>, text: String, textColor: Color = Color.gray900, size: CGSize = CGSize(width: BaseWidth.w32.value, height: BaseHeight.h32.value)) {
+    public init(isAnimating: Binding<Bool>,
+                text: String,
+                textColor: Color = Color.gray900,
+                indicatorColor: UIColor = UIColor.gray900,
+                size: CGSize = CGSize(width: BaseWidth.w32.value, height: BaseHeight.h32.value)) {
         self._isAnimating = isAnimating
         self.text = text
         self.textColor = textColor
+        self.indicatorColor = indicatorColor
         self.size = size
     }
     
@@ -27,7 +33,7 @@ public struct LoadingView: View {
                 .font(.system(size: .bodyEmphasis, weight: .semibold))
             
             HStack {
-                SwiftUINVActivityIndicatorView(isAnimating: self.$isAnimating, size: size, type: .ballPulseSync)
+                SwiftUINVActivityIndicatorView(isAnimating: self.$isAnimating, size: size, type: .ballPulseSync, color: indicatorColor)
                     .frame(maxWidth: .w32, minHeight: .h32, maxHeight: .h32)
             }
         }
